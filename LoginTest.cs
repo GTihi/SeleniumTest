63e867f66798984d2a74fed635c3256095189cb1
gagyi <gagyi@RANGERR>
2025-03-14 14:29:41 +0200

    Add project files.

---
+++ LoginTest.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+public class LoginTest
+{
+    [Fact]
+    public void LoginToWebsite()
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("http://seleniumtests.scienceontheweb.net/wp-admin");
+
+        driver.FindElement(By.Id("user_login")).SendKeys("testuser");
+        driver.FindElement(By.Id("user_pass")).SendKeys("Tests3cret!");
+        driver.FindElement(By.Id("wp-submit")).Click();
+
+        Assert.Contains("Dashboard", driver.PageSource);
+        driver.Quit();
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Threading;
+using Xunit;
+
+public class SeleniumTest : IDisposable
+{
+    private readonly IWebDriver driver;
+
+    public SeleniumTest()
+    {
+        driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("http://seleniumtests.scienceontheweb.net/");
+        Thread.Sleep(2000);
+    }
+
+    [Fact]
+    public void TestPageTitle()
+    {
+        string expectedTitle = "Selenium Webdriver Tests";
+        Assert.Contains(expectedTitle, driver.Title);
+    }
+
+    [Fact]
+    public void TestButtonClick()
+    {
+
+        IWebElement articleLink = driver.FindElement(By.CssSelector("article#post-14 h2.entry-title a"));
+        articleLink.Click();
+        Thread.Sleep(2000);
+        Assert.Equal("Sincronizare în Selenium – gestionarea Wait-urilor – Selenium Webdriver Tests", driver.Title);
+
+    }
+
+    [Fact]
+    public void TestButtonClickFail()
+    {
+
+        IWebElement articleLink = driver.FindElement(By.CssSelector("article#post-11 h2.entry-title a"));
+        articleLink.Click();
+        Thread.Sleep(2000);
+        Assert.NotEqual("Sincronizare în Selenium – gestionarea Wait-urilor – Selenium Webdriver Tests", driver.Title);
+
+    }
+
+    [Fact]
+    public void TestNextPostButtonClick()
+    {
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement articleLink = driver.FindElement(By.CssSelector("article#post-11 h2.entry-title a"));
+        articleLink.Click();
+        IWebElement nextPostLink = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".nav-next a")));
+        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", nextPostLink);
+        Thread.Sleep(2000);
+        nextPostLink.Click();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        Assert.NotEqual("Interacțiunea cu elementele web în Selenium WebDriver – Selenium Webdriver Tests", driver.Title);
+
+    }
+
+
+
+
+    public void Dispose()
+    {
+        driver.Quit();
+    }
+}
